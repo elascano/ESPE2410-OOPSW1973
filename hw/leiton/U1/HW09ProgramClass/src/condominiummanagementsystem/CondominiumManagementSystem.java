be8503c548ac04fd2187ce62e2c516e98ccd0511
JOSE IGNACIO LEITON HARO <jileit@hotmail.com>
2024-11-14 12:15:31 -0500

    add project condominium and chicken

---
+++ hw/leiton/U1/HW09ProgramClass/src/condominiummanagementsystem/CondominiumManagementSystem.java
@@ -0,0 +1,28 @@
+package condominiummanagementsystem;
+
+import ec.espe.edu.Administrator;
+import ec.espe.edu.Condominium;
+import ec.espe.edu.House;
+import ec.espe.edu.Owner;
+
+
+/**
+ *
+ * @author Jose Leiton
+ * @version 0.1.0
+ * @since 2024
+ */
+public class CondominiumManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Houses");
+        House house = new House();
+        Administrator administrator = new Administrator();
+        Condominium condominium = new Condominium();
+        Owner owner = new Owner();
+        
+    }
+}
