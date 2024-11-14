2d1ab1a7d1d0e9cf0629d5d4948ab020ee037596
Pilatasig Quinaluisa David Ariel <david_pilatasig@hotmail.com>
2024-11-14 07:21:53 -0500

    Homework9 V1.0

---
+++ hw/pilatasig/u1/HW_09_Class_Project/Notification.java
@@ -0,0 +1,12 @@
+package ec.espe.edu.model;
+import java.util.Date;
+/**
+ *
+ * @author David Pilatasig
+ */
+public class Notification {
+    int notificationId;
+    Date currentDate;
+    String message;
+    String kindOfMessage;
+}
