1425b7832ee631dae7a1e9519acf6a7d7ca043ed
Robinson Bonilla <robinsonedubojaja@gmail.com>
2024-11-20 00:48:02 -0500

    Agregar nuevos archivos en ws/bonilla/U1/

---
+++ ws/bonilla/U1/SW_02_PrjChickenSinulator/PrjChickenSimulato/src/ec/espe/edu/model/Chicken.java
@@ -0,0 +1,17 @@
+
+package ec.espe.edu.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author HP
+ */
+public class Chicken {
+    int id;
+    String name;
+    String color;
+    int age;
+    Date bornOnDate;
+    boolean notMolting;
+}
