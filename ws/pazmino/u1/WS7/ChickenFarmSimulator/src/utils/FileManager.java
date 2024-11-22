f6b37a54d7e8ab63602de9d9066423062e53e902
Brandon <pazminob558@gmail.com>
2024-11-22 14:29:05 -0500

    WS7CHickenFarmSimulator

---
+++ ws/pazmino/u1/WS7/ChickenFarmSimulator/src/utils/FileManager.java
@@ -0,0 +1,12 @@
+
+package utils;
+
+/**
+ *
+ * @author Brandon Pazmino
+ */
+public class FileManager {
+    public static void save(String data,String fileName, String format) {
+        
+    }
+}
