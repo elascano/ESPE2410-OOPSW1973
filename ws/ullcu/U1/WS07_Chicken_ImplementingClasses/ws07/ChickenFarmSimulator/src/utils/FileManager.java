9e0f1de412ee8b1766452cccbc665b523fabd662
Ing. Alex <daullcu@espe.edu.ec>
2024-11-24 17:24:09 -0500

    FolderUpdate

---
+++ ws/ullcu/U1/WS07_Chicken_ImplementingClasses/ws07/ChickenFarmSimulator/src/utils/FileManager.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ *
+ * @author Andres Sandoval
+ */
+public class FileManager {
+    
+    public static void save(String data, String fileName, String format){
+        
+        
+    }
+    
+}
