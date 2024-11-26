fbdb78e2262d07fd9e01139baaa5c0fbf6f568fd
Brandon Pazmino <pazminob558@gmail.com>
2024-11-26 13:01:51 -0500

    WS8COntacts

--- ws/pazmino/u1/WS7/ChickenFarmSimulator/src/utils/FileManager.java
+++ ws/pazmino/u1/WS7/ChickenFarmSimulator/src/utils/FileManager.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package utils;
+ package utils;
 
 /**
  *
  * @author Brandon Pazmino <www.espe.edu.ec>
  */
 public class FileManager {
+    public static void save(String data, String fileName,String format) {
+        
+    }
     
 }
---
+++ ws/pazmino/u1/WS8/PrjContactBook/src/ec/edu/espe/contacts/model/Contact.java
@@ -0,0 +1,135 @@
+package ec.edu.espe.contacts.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Brandon Pazmino <www.espe.edu.ec>
+ */
+public class Contact {
+    private int id;
+    private String name;
+    private Date birthDate;
+    private String email;
+    private int numberOfMutualFriends;
+    private int numberOfOtherFriends;
+    private int totalFriends;
+      public Contact(int id, String name, Date birthDate, String email, int numberOfMutualFriends, int numberOfOtherFriends, int totalFriends) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.numberOfMutualFriends = numberOfMutualFriends;
+        this.numberOfOtherFriends = numberOfOtherFriends;
+        this.totalFriends = totalFriends;
+    }
+      
+      
+        @Override
+    public String toString() {
+        return "Contact{" + "id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", email=" + email + ", numberOfMutualFriends=" + numberOfMutualFriends + ", numberOfOtherFriends=" + numberOfOtherFriends + ", totalFriends=" + totalFriends + '}';
+    }
+
+
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the birthDate
+     */
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    /**
+     * @param birthDate the birthDate to set
+     */
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the numberOfMutualFriends
+     */
+    public int getNumberOfMutualFriends() {
+        return numberOfMutualFriends;
+    }
+
+    /**
+     * @param numberOfMutualFriends the numberOfMutualFriends to set
+     */
+    public void setNumberOfMutualFriends(int numberOfMutualFriends) {
+        this.numberOfMutualFriends = numberOfMutualFriends;
+    }
+
+    /**
+     * @return the numberOfOtherFriends
+     */
+    public int getNumberOfOtherFriends() {
+        return numberOfOtherFriends;
+    }
+
+    /**
+     * @param numberOfOtherFriends the numberOfOtherFriends to set
+     */
+    public void setNumberOfOtherFriends(int numberOfOtherFriends) {
+        this.numberOfOtherFriends = numberOfOtherFriends;
+    }
+
+    /**
+     * @return the totalFriends
+     */
+    public int getTotalFriends() {
+        return totalFriends;
+    }
+
+    /**
+     * @param totalFriends the totalFriends to set
+     */
+    public void setTotalFriends(int totalFriends) {
+        this.totalFriends = totalFriends;
+    }
+    
+    
+    
+}
