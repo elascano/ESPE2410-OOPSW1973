f223abbc3a1b3a8b633293acf1fc4eca5dd7e1f3
Cuichan_Hector <hdcuichan@espe.edu.ec>
2024-11-28 15:29:33 -0500

    WS Update

---
+++ ws/cuichan/U1/WS09_ArrayList/PrjContacts/src/ec/edu/espe/contacts/model/Person.java
@@ -0,0 +1,58 @@
+package ec.edu.espe.contacts.model;
+
+/**
+ *
+ * @author Cahd!
+ */
+public class Person {
+    private int id;
+    private String name;
+    private String lastName;
+    private float salary;
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + ", lastName=" + lastName + ", salary=" + salary + "}\n";
+    }
+
+    public Person(int id, String name, String lastName, float salary) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    
+}
---
+++ ws/cuichan/U1/WS09_ArrayList/PrjContacts/src/ec/edu/espe/contacts/view/MyContacts.java
@@ -0,0 +1,60 @@
+package ec.edu.espe.contacts.view;
+
+import ec.edu.espe.contacts.model.Person;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ *
+ * @author Cahd!
+ */
+public class MyContacts {
+    public static void main(String[] args) {
+        System.out.println("--> My Book Contacts <--");
+        int id = 1;
+        String name = "David";
+        String lastName = "Cuichan";
+        float salary = 4078.0F;
+        
+        Person person;
+        person = new Person(id, name, lastName, salary);
+        
+        Collection things;
+        things = new ArrayList<>();
+        
+        things.add(2);
+        things.add(234.45F);
+        things.add('A');
+        things.add(true);
+        things.add("David Cuichan");
+        things.add(person);
+        
+        System.out.println("things --> " + things);
+        things.add(new Date());
+        things.add(id);
+        System.out.println("things--> " + things);
+        
+        Collection<Person>persons;
+        persons = new ArrayList<>();
+        for(int i=0; i<5; i++){
+            persons.add(new Person(i+1, "David" + (i+1), "Cuichan" + (i+1),(i+1)*1000));
+        }
+        persons.forEach((p)->{
+            System.out.println("Person --> " + p);
+        });
+        
+        System.out.println("persons --> " + persons);
+        
+        int setOfNumbers[] = {1,6,9,4,6,-12};
+        System.out.println("setOfNumbers size --> " + setOfNumbers.length);
+        
+        for(int n:setOfNumbers){
+            System.out.println("no. --> " + n);;
+        }
+        
+        for(Person p:persons){
+            System.out.println("person --> " + p);
+        }
+        }
+}
