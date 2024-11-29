c76b7d3e0cec97339793c3ec38faa27d2fdf33a7
MPasquel2202 <marlonpas2202@outlook.com>
2024-11-28 19:45:44 -0500

    Ws 09

--- ws/pasquel/u1/Ws no9 Array list/PrjContact/src/ec/edu/espe/prjcontact/view/Mycontacts.java
+++ ws/pasquel/u1/Ws no9 Array list/PrjContact/src/ec/edu/espe/prjcontact/view/Mycontacts.java
@@ -1,4 +1,3 @@
-
 package ec.edu.espe.prjcontact.view;
 
 import ec.edu.espe.prjcontact.model.Person;
@@ -9,64 +8,50 @@ import java.util.Collection;
  *
  * @author marlo
  */
-public class Mycontacts {
+public class MyContacts {
     public static void main(String[] args) {
         System.out.println("My book of contacts");
-        int id=1;
-        String name="Marlon";
-        String lastname="Pasquel";
-        float salary=4878F;
         
-        Person person;
-        person = new Person(id, name, lastname, salary);
-       Collection things;
-       things= new ArrayList;
-       things.add(2);
-       things.add(834.45F);
-       things.add("A");
-       things.add(true);
-       things.add("Marlon Pasquel");
-       things.add(person);
-       
-        System.out.println("things----> "+ things);
-        Collection <Persons>persons;
-        persons = new ArrayList<>();
-        for (int i = 0; i<5 ; i++-){
-        persons.add(new Person(i+1, "marlon" + (i+1), "Pasquel"+ (i+1), (i+1)*1000))
-        }
-        persons.forEach((p)->{
-           System.out.println("person--> "+p);}
-        );
-        System.out.println("persons-->"+ persons);
-         int setOfNumbers[] = {1,6,9,4,6,0,-12};
+        int id = 1;
+        String name = "Marlon";
+        String lastname = "Pasquel";
+        float salary = 4878F;
         
-        System.out.println("setOfNumbers size -->" + setOfNumbers.length);
+        Person person = new Person(id, name, lastname, salary);
         
-        for (int n:setOfNumbers){
-            System.out.println("no. -->" + n);
-   
-        }
-        for (Person p:persons){
-            System.out.println("person -->" + p);
-        }
-        for (int i= 0 ; i<persons.size();i++ ){
-            
-        } int setOfNumbers[] = {1,6,9,4,6,0,-12};
+        Collection<Object> things = new ArrayList<>(); 
+        things.add(2);
+        things.add(834.45F);
+        things.add("A");
+        things.add(true);
+        things.add("Marlon Pasquel");
+        things.add(person);
         
-        System.out.println("setOfNumbers size -->" + setOfNumbers.length);
+        System.out.println("things----> " + things);
         
-        for (int n:setOfNumbers){
-            System.out.println("no. -->" + n);
-   
+        Collection<Person> persons = new ArrayList<>();
+        
+        for (int i = 0; i < 5; i++) {
+            persons.add(new Person(i + 1, "Marlon" + (i + 1), "Pasquel" + (i + 1), (i + 1) * 1000));
         }
-        for (Person p:persons){
-            System.out.println("person -->" + p);
+        
+        persons.forEach((p) -> {
+            System.out.println("person--> " + p);
+        });
+        
+        System.out.println("persons--> " + persons);
+        
+        int[] setOfNumbers = {1, 6, 9, 4, 6, 0, -12};
+        
+        System.out.println("setOfNumbers size --> " + setOfNumbers.length);
+        
+        for (int n : setOfNumbers) {
+            System.out.println("no. --> " + n);
         }
-        for (int i= 0 ; i<persons.size();i++ ){
-            
+        
+        for (Person p : persons) {
+            System.out.println("person --> " + p);
         }
-       
-                    
-    
     }
 }
+
