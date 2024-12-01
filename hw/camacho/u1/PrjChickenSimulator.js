0c995bc55deb492a8b48f31339f6a17fca13f3ad
Camacho-Mateo <mateomarucha@gmail.com>
2024-12-01 17:50:44 -0500

    new documents

---
+++ hw/camacho/u1/PrjChickenSimulator.js
@@ -0,0 +1,17 @@
+console.log("Hello world");
+class Chicken {
+    constructor() {
+        this.id = 1;
+        this.name = '';
+        this.color = '';
+        this.age = 0;
+        this.bornOnDate = new Date();
+        this.notMolting = true;
+    }
+}
+
+console.log("Hello world");
+
+const chicken = new Chicken(); 
+console.log("Chicken---> " + chicken.toString());
+
