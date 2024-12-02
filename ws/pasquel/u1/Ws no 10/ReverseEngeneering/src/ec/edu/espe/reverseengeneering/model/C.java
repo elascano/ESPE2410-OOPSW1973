66070b03c74d5ae78f05c6f04dfaddd737836680
MPasquel2202 <marlonpas2202@outlook.com>
2024-12-02 14:59:23 -0500

    Ws 10

---
+++ ws/pasquel/u1/Ws no 10/ReverseEngeneering/src/ec/edu/espe/reverseengeneering/model/C.java
@@ -0,0 +1,15 @@
+
+package ec.edu.espe.reverseengeneering.model;
+
+/**
+ *
+ * @author marlo
+ */
+public class C {
+
+    @Override
+    public String toString() {
+        return "C{" + '}';
+    }
+    
+}
