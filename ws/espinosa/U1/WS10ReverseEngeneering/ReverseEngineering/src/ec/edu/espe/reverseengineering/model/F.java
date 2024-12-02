9056241d4a3f1e6f36efc41ffa92a89ca6f37e69
Andres-Espinosa-18 <aeespinosa6@espe.edu.ec>
2024-12-02 15:00:30 -0500

    WS10

---
+++ ws/espinosa/U1/WS10ReverseEngeneering/ReverseEngineering/src/ec/edu/espe/reverseengineering/model/F.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.reverseengineering.model;
+
+/**
+ *
+ * @author Andrés Espinosa
+ */
+public class F {
+    
+    public void m(C c){
+        System.out.println("c inside m -->" + c);
+    }
+}
