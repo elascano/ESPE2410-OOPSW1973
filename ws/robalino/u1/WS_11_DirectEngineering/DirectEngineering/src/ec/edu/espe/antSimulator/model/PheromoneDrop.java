6fa84205b8c2e7ee4c40d57409e401ef82520ff1
Benjamin Robalino <abrobalino@espe.edu.ec>
2024-12-03 13:12:21 -0500

    ws 11

---
+++ ws/robalino/u1/WS_11_DirectEngineering/DirectEngineering/src/ec/edu/espe/antSimulator/model/PheromoneDrop.java
@@ -0,0 +1,15 @@
+
+
+package ec.edu.espe.antSimulator.model;
+
+/**
+ *
+ * @author LABS-DCCO
+ */
+public class PheromoneDrop {
+    int currentLevel;
+    
+    public void run (){
+        System.out.println(" the Cell is runing....");
+    }
+}
