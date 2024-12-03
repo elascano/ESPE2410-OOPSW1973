4f01b727931a4d2974ece7ab89d529e981616b96
Dennis Paucar <dennispaucar49@gmail.com>
2024-12-03 12:59:13 -0500

    WS10 AntSimulator

---
+++ ws/paucar/u1/WS11/AntSimulator/src/ec/edu/espe/antsimulator/model/PheromoneDrop.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.antsimulator.model;
+
+/**
+ *
+ * @author Marlon Pasquel
+ */
+public class PheromoneDrop {
+    int currentLevel;
+    
+    public void run(){
+        System.out.println(" The cell is running ");
+    }
+}
