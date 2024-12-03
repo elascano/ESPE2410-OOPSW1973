1844a96839e3592c0c11b28b3b6c115d5b8188c9
GongoraLucas <gongoralucas59@gmail.com>
2024-12-03 15:00:25 -0500

    added WS11

---
+++ ws/gongora/U1/WS11/AntSimulator/src/ec/edu/espe/antsimulator/model/PheromoneDrop.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.antsimulator.model;
+
+/**
+ *
+ * @author Lucas Gongora
+ */
+public class PheromoneDrop {
+
+    int currentLevel;
+
+    public void run() {
+        System.out.println(" the CELL is running ...");
+    }
+}
---
+++ ws/gongora/U1/WS11/AntSimulator/src/ec/edu/espe/antsimulator/view/AntSimulator.java
@@ -0,0 +1,32 @@
+package ec.edu.espe.antsimulator.view;
+
+import ec.edu.espe.antsimulator.model.Ant;
+import ec.edu.espe.antsimulator.model.Cell;
+import ec.edu.espe.antsimulator.model.Colony;
+import ec.edu.espe.antsimulator.model.Food;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lucas Gongora
+ */
+public class AntSimulator {
+
+    public static void main(String[] args) {
+        Ant ant;
+        Cell cell;
+        Food food;
+        
+        food = new Food(3);
+        cell = new Cell(0,0);
+        ant = new Ant(cell,0);
+        
+        ArrayList<Ant> ants;
+        ants = null;
+        Colony colony = new Colony(ants);
+        
+        
+        colony.run();
+        ant.carry(food);
+    }
+}
