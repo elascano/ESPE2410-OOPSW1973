e22ca58075e7528eb18c4918d68b88f91c17b147
Cuichan_Hector <hdcuichan@espe.edu.ec>
2024-12-03 21:11:26 -0500

    WS11 V1.0.0

---
+++ ws/cuichan/U1/WS11_DirectEngineering/AntSimulator/src/ec/edu/espe/antsimulator/model/Area.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.antsimulator.model;
+
+/**
+ *
+ * @author LABS-DCCO
+ */
+public class Area {
+
+}
---
+++ ws/cuichan/U1/WS11_DirectEngineering/AntSimulator/src/ec/edu/espe/antsimulator/view/colony.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.antsimulator.view;
+
+/**
+ *
+ * @author LABS-DCCO
+ */
+public class colony {
+
+    public colony() {
+    }
+
+}
