b98d6c39765eb7db29a83ca0fccc61c699870465
Camila Salome Bohórquez Alava <camilabohorquez512@hotmail.com>
2024-12-08 20:53:17 -0500

    WS_10_Revers_engineer

---
+++ ws/WS_10_REVERS ENGINNER/ReverseEnginering/src/ec/edu/espe/reverseengineering/model/F.java
@@ -0,0 +1,11 @@
+package ec.edu.espe.reverseengineering.model;
+
+/**
+ *
+ * @author Camila Bohorquez 
+ */
+public class F {
+    public void m(C c){
+        System.out.println("c inside m-->"+c);
+    }
+}
---
+++ ws/WS_10_REVERS ENGINNER/ReverseEnginering/src/reverseengineering/ReverseEnginering.java
@@ -0,0 +1,16 @@
+package reverseengineering;
+
+/**
+ *
+ * @author Camila Bohorquez
+ */
+public class ReverseEnginering {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
