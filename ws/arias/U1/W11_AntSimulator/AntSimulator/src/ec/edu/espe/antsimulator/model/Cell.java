f995582fc9f6921c3bf3caba21d19d1613ca45af
Alan <ajarias4@espe.edu.ec>
2024-12-10 10:51:23 -0500

    WS 11 evidence

---
+++ ws/arias/U1/W11_AntSimulator/AntSimulator/src/ec/edu/espe/antsimulator/model/Cell.java
@@ -0,0 +1,25 @@
+package ec.edu.espe.antsimulator.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Alan Arias
+ */
+public class Cell {
+    int col;
+    int row;
+    public void receivePheromone(PheromoneDrop pheromoneDrop){
+        System.out.println("the cell is receiving a pheromone drop");
+    }
+    public ArrayList<Ant> getAnts(){
+        
+        return new ArrayList<Ant>();
+    }
+
+    public Cell(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
+    
+}
