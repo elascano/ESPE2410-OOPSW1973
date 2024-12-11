da5d7e24c71c2f064e0e1a5ac418c0684e243f8a
rojasmatias18 <mati2006rojas@gmail.com>
2024-12-10 19:01:33 -0500

    Hw Fd

---
+++ hw/rojas/U1/HW09/Admin.java/src/ec/espe/edu/model/Admin.java
@@ -0,0 +1,13 @@
+package ec.espe.edu.model;
+
+/**
+ *
+ * @author Matias Rojas
+ */
+public class Admin {
+    String name;
+    int id;
+    int experience;
+    int phoneNumber;
+    float salary;
+}
