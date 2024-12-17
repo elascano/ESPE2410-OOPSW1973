e41c9522af4c18a4b11b4f11d46203b58a7448f1
GongoraLucas <gongoralucas59@gmail.com>
2024-12-16 23:40:00 -0500

    added HW17

---
+++ hw/gongora/U2/HW17CreateAnObjectFile/java/Cable/src/ec/edu/espe/cable/view/CableApp.java
@@ -0,0 +1,15 @@
+package ec.edu.espe.cable.view;
+
+import ec.edu.espe.cable.model.Menu;
+
+/**
+ *
+ * @author Lucas Gongora
+ */
+public class CableApp {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.runMenu();
+    }
+}
