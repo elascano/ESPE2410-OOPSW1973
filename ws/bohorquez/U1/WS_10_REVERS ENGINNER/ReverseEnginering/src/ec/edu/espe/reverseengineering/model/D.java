31d23a7e48e9b08e4a58b164a501278d2b731144
Camila Salome Bohórquez Alava <camilabohorquez512@hotmail.com>
2025-01-10 09:16:08 -0500

    Ordenar carpetas

---
+++ ws/bohorquez/U1/WS_10_REVERS ENGINNER/ReverseEnginering/src/ec/edu/espe/reverseengineering/model/D.java
@@ -0,0 +1,11 @@
+package ec.edu.espe.reverseengineering.model;
+
+/**
+ *
+ * @author Camila Bohorquez
+ */
+public class D {
+    public E m(){
+        return new E(5,0.1F,'a',"Hola Camila");
+    }   
+}
