d53fee46039b7c774d3aaec217d7db9369004fdc
Esteban Quiroga <quirogaesteban1@gmail.com>
2025-01-13 16:14:58 -0500

    hw 22-24

--- ws/quiroga/u2/WS_18_GUI/ContactBook/src/main/java/ec/edu/espe/contactbook/view/FrmContact.java
+++ ws/quiroga/u2/WS_18_GUI/ContactBook/src/main/java/ec/edu/espe/contactbook/view/FrmContact.java
@@ -244,7 +244,7 @@ public class FrmContact extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_btnAddActionPerformed
 
     /**
