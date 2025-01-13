2bf4d7191dc47215374c0d6a162357d36bd6c85c
Pilatasig Quinaluisa David Ariel <david_pilatasig@hotmail.com>
2025-01-13 13:31:15 -0500

    Something

---
+++ ws/pilatasig/u2/Taller hacer hoy/mavenproject1/src/main/java/ec/edu/espe/mavenproject1/Mavenproject1.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.mavenproject1;
+
+/**
+ *
+ * @author David Pilatasig
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
