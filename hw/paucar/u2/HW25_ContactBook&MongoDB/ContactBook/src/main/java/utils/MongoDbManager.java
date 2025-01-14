31ee9081c9c73de5deed901dce49bf18231126e1
DennisPaucar <dennispaucar49@gmail.com>
2025-01-14 06:18:29 -0500

    HW25

---
+++ hw/paucar/u2/HW25_ContactBook&MongoDB/ContactBook/src/main/java/utils/MongoDbManager.java
@@ -0,0 +1,66 @@
+package utils;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+
+public class MongoDbManager {
+
+    private static final String CONNECTION_STRING = "mongodb+srv://dspaucar:dspaucar@cluster0.lx1js.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+    private static MongoClient mongoClient;
+    private static MongoDatabase database;
+
+    
+    static {
+        try {
+            
+            mongoClient = MongoClients.create(CONNECTION_STRING);
+            
+            database = mongoClient.getDatabase("oop");
+            System.out.println("Connected to MongoDB database: oop");
+        } catch (Exception e) {
+            System.err.println("Error connecting to MongoDB: " + e.getMessage());
+        }
+    }
+
+   
+    public static boolean insert(String collectionName, Document document) {
+        try {
+            
+            MongoCollection<Document> collection = database.getCollection(collectionName);
+            
+            collection.insertOne(document);
+            System.out.println("Document inserted into collection: " + collectionName);
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error inserting document: " + e.getMessage());
+            return false;
+        }
+    }
+
+    
+    public static void showAll(String collectionName) {
+        try {
+            
+            MongoCollection<Document> collection = database.getCollection(collectionName);
+            
+            System.out.println("\nDocuments in collection: " + collectionName);
+            for (Document doc : collection.find()) {
+                System.out.println(doc.toJson());
+            }
+        } catch (Exception e) {
+            System.err.println("Error retrieving documents: " + e.getMessage());
+        }
+    }
+
+    
+    public static void closeConnection() {
+        if (mongoClient != null) {
+            mongoClient.close();
+            System.out.println("MongoDB connection closed.");
+        }
+    }
+}
