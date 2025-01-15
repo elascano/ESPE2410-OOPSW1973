3bb0137bdd81812c997d950f8850b32c83259a74
Andres-Espinosa-18 <aeespinosa6@espe.edu.ec>
2025-01-15 13:11:26 -0500

    WS22

---
+++ ws/espinosa/U2/WS22/Farm/src/main/java/ec/edu/espe/farm/model/FarmAnimal.java
@@ -0,0 +1,27 @@
+package ec.edu.espe.farm.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author Andrés Espinosa
+ */
+public abstract class FarmAnimal {
+    int id;
+    String breed;
+    Date bornonDate;
+    
+    ArrayList<FoodType> foodType;
+    
+    public int getAgeinMonts(){
+            //TODO
+            return 0;
+    }
+    
+    public abstract FoodType (){
+        //TODO
+        return FoodType foodtype;
+    }
+    public
+}
---
+++ ws/espinosa/U2/WS22/Farm/src/main/java/ec/edu/espe/farm/view/Farm.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.farm.view;
+
+import ec.edu.espe.farm.model.FarmAnimal;
+
+/**
+ *
+ * @author Andrés Espinosa
+ */
+public class Farm {
+    public static void main(String[] args) {
+        FarmAnimal farmAnimal;
+        farmAnimal = new FarmAnimal();
+    }
+}
