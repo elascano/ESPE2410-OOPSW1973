67b13143060653086772ccdb618c1e38fdff2c6a
Brandon <pazminob558@gmail.com>
2025-01-18 22:43:32 -0500

    updmerge

---
+++ ws/pazmino/u2/WS23Abstraction/Farm/src/main/java/ec/edu/espe/farm/model/Product.java
@@ -0,0 +1,36 @@
+package ec.edu.espe.farm.model;
+
+/**
+ *
+ * @author Brandon Pazmino
+ */
+public class Product {
+
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Product{" + "description=" + description + '}';
+    }
+    
+
+    public Product(String description) {
+        this.description = description;
+    }
+    
+    
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
