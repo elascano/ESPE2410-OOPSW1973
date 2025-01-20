ca944398d7771c84bf1ee30de19a3de6311890c2
Cuichan_Hector <hdcuichan@espe.edu.ec>
2025-01-20 18:08:56 -0500

    WS Update

---
+++ hw/cuichan/U2/HW17-SavingObjects/JavaMongoDB/src/main/java/ec/edu/espe/javamongoDB/view/JavaMongoDB.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.javamongoDB.view;
+
+/**
+ *
+ * @author David Cuichan
+ */
+public class JavaMongoDB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
