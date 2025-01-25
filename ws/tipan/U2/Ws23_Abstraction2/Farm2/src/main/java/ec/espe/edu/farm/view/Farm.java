06939a4535ce136c3c8e5bfbc1dc23131581a9ce
LizTipan <lisbethtipan47@gmail.com>
2025-01-25 00:55:39 -0500

    Evidence Hw and Ws

---
+++ ws/tipan/U2/Ws23_Abstraction2/Farm2/src/main/java/ec/espe/edu/farm/view/Farm.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.espe.edu.farm.view;
+
+import ec.espe.edu.farm.model.FarmAnimal;
+
+/**
+ *
+ * @author Lisbeth Tipan
+ */
+public class Farm {
+    public static void main(String[] args){
+        FarmAnimal farmAnimal;
+        farmAnimal= new FarmAnimal();
+    }
+}
