9a9de6081a3fd9294e83d3ffaa27e5ad2f6fd8ef
Alan Joel Arias Iza <ariasalan634@gmail.com>
2025-01-27 13:12:54 -0500

    Added EXAM evidences

---
+++ EXAM/arias/u2/Q38_51/src/main/java/espe/edu/ec/q38/model/B.java
@@ -0,0 +1,35 @@
+package espe.edu.ec.q38.model;
+import java.util.ArrayList;
+
+/**
+ *
+ * @Alan Arias
+ */
+public class B extends A{
+    
+    private ArrayList <H> h = new ArrayList<>(1);
+    public B(ArrayList<A> as) {
+        super(as);
+    }
+
+    @Override
+    public String toString() {
+        return "B{" + "h=" + h + '}';
+    }
+    
+    /**
+     * @return the h
+     */
+    public ArrayList<H> getH() {
+        return h;
+    }
+
+    /**
+     * @param h the h to set
+     */
+    public void setH(ArrayList<H> h) {
+        this.h = h;
+    }
+    
+    
+}
