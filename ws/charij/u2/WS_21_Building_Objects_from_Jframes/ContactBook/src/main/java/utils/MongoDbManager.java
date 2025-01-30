6a7f8dbc6fec9f5b2ce68abcf3bac2da09c01b54
JAIR SEBASTIAN CHARIJ RAURA <jairsebastian2005@hotmail.com>
2025-01-29 21:28:47 -0500

    WS

---
+++ ws/charij/u2/WS_21_Building_Objects_from_Jframes/ContactBook/src/main/java/utils/MongoDbManager.java
@@ -0,0 +1,28 @@
+
+
+package utils;
+
+/**
+ *
+ * @author Sebastian Charij
+ */
+public class MongoDbManager {
+    public static void connect(String connectionString){
+        System.out.println("connecting...");
+    }
+    public static boolean add(Object object){
+        //TODO add any object to the database
+        String collectionName = null;
+        boolean saved = false;
+        try{
+        collectionName = object.getClass().getCanonicalName();
+        System.out.println("saving --> "+collectionName+" <--- "+object);
+        //Implement the saving data for any object.
+        saved = true;
+        }catch(Exception ex){
+            System.out.println("error on saving " +collectionName+ "");
+            saved = false;
+        }
+        return saved;
+    }
+}
