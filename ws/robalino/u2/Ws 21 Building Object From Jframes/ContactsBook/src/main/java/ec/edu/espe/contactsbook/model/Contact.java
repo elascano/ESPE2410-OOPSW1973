5f0c9ee8ba2546604bfdcd101951e35747834f8f
Benjamin Robalino <benjamin_robalino@yahoo.com>
2025-01-30 12:22:21 -0500

    Hw Ws Wx

---
+++ ws/robalino/u2/Ws 21 Building Object From Jframes/ContactsBook/src/main/java/ec/edu/espe/contactsbook/model/Contact.java
@@ -0,0 +1,159 @@
+
+
+package ec.edu.espe.contactsbook.model;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+/**
+ *
+ * @author Benjamin R
+ */
+public class Contact {
+    private int id;
+    private String name;
+    private char sex;
+    private String gender;
+    private String maritalStatus;
+    private Calendar dateOfBirth;
+    private ArrayList<String> favoriteSports;
+    private String comments;
+
+    @Override
+    public String toString() {
+        return  "Id: " + id + "\n Name: " + name + "\n Sex: " + sex + "\n Gender: " + gender + "\n Marital status: " + maritalStatus + "\n Date of birth: " + dateOfBirth + "\n Favorite sports:" + favoriteSports + "\n Comments: " + comments ;
+    }
+    
+    
+
+    public Contact(int id, String name, char sex, String gender, String maritalStatus, Calendar dateOfBirth, ArrayList<String> favoriteSports, String comments) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.gender = gender;
+        this.maritalStatus = maritalStatus;
+        this.dateOfBirth = dateOfBirth;
+        this.favoriteSports = favoriteSports;
+        this.comments = comments;
+    }
+    
+    
+    
+    
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the sex
+     */
+    public char getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex the sex to set
+     */
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    /**
+     * @return the gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the maritalStatus
+     */
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    /**
+     * @param maritalStatus the maritalStatus to set
+     */
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    /**
+     * @return the dateOfBirth
+     */
+    public Calendar getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    /**
+     * @param dateOfBirth the dateOfBirth to set
+     */
+    public void setDateOfBirth(Calendar dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * @return the favoriteSports
+     */
+    public ArrayList<String> getFavoriteSports() {
+        return favoriteSports;
+    }
+
+    /**
+     * @param favoriteSports the favoriteSports to set
+     */
+    public void setFavoriteSports(ArrayList<String> favoriteSports) {
+        this.favoriteSports = favoriteSports;
+    }
+
+    /**
+     * @return the comments
+     */
+    public String getComments() {
+        return comments;
+    }
+
+    /**
+     * @param comments the comments to set
+     */
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+    
+    
+    
+}
