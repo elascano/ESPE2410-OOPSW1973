f2a8f00b368ae203e47a47da488a1c61307b22df
unknown <mateotopon7@gmail.com>
2025-01-29 19:05:44 -0500

    exam

---
+++ EXAM/topon/u2/q38_51 MT/q38_51/src/main/java/ec/edu/espe/q38_51/view/Q38_51.java
@@ -0,0 +1,30 @@
+package ec.edu.espe.q38_51.view;
+import ec.edu.espe.q38.model.*;
+
+/**
+ *
+ * @author Mateo Topon
+ */
+public class Q38_51 {
+    public static void main(String[] args) {
+        A a = new A(new A());
+        B b = new B();
+        C c = new C();
+        D d = new D();
+        E e = new E();
+        F f= new F();
+        G g= new G();
+        H h = null;
+        J j = new J();
+        
+        System.out.println("A --->" + a);
+        System.out.println("B --->" + b);
+        System.out.println("C --->" + c);
+        System.out.println("D --->" + d);
+        System.out.println("E --->" + e);
+        System.out.println("F --->" + f);
+        System.out.println("G --->" + g);
+        System.out.println("H --->" + h);
+        System.out.println("J --->" + j);
+    }
+}
