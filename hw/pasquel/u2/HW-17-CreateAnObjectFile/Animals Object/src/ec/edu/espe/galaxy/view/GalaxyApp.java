2956c9b9cf8a293afcb08ec09d3973046a2356f1
MPasquel2202 <marlonpas2202@outlook.com>
2025-01-30 12:03:42 -0500

    HW

---
+++ hw/pasquel/u2/HW-17-CreateAnObjectFile/Animals Object/src/ec/edu/espe/galaxy/view/GalaxyApp.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.galaxy.view;
+
+import static ec.edu.espe.galaxy.view.GalaxyManager.showMenuOptions;
+
+/**
+ *
+ * @author Alan Arias
+ */
+public class GalaxyApp {
+    public static void main(String[] args) {
+        showMenuOptions();
+    }
+}
