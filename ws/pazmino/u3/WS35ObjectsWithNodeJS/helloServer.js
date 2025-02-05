edfb254591295f782c7343d8297f68c7c90e697f
Brandon Pazmino <pazminob558@gmail.com>
2025-02-05 13:04:11 -0500

    WS35JSURIU

--- ws/pazmino/u3/WS35ObjectsWithNodeJS/helloServer.js
+++ ws/pazmino/u3/WS35ObjectsWithNodeJS/helloServer.js
@@ -3,7 +3,7 @@ const hostname = '127.0.0.1';
 const port = 3015;
 const server = http.createServer((req,res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain'); 
+    res.setHeader('Content-Type', 'text/plain'); // text/html 
     res.end('Hello, <b>Web Developers!</b> from <i>Brandon Pazmino</i>');    
 });
 server.listen(port,hostname, ( ) => {
