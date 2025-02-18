e353c5d0e1b8d40b5c44fca775265e4263e33b29
Ing. Alex <daullcu@espe.edu.ec>
2025-02-18 08:18:30 -0500

    HW35

---
+++ hw/ullcu/U3/HW_35_Singleton/Singleton/src/main/java/ec/edu/espe/view/TaxView.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.view;
+
+/**
+ *
+ * @author Alexander Ullco
+ */
+
+public class TaxView {
+    public void showTotal(double total) {
+        System.out.println("Total with tax: $" + total);
+    }
+}
