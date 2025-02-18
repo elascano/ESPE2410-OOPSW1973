775afe7d282901715bd008cefd88b67a531c7855
Jardel Alexander Maza Flores <2432jardel.maza@gmail.com>
2025-02-17 20:36:16 -0500

    HW35

---
+++ hw/maza/u3/HW35/Singleton/src/main/java/ec/edu/espe/view/Singleton.java
@@ -0,0 +1,26 @@
+package ec.edu.espe.view;
+
+/**
+ *
+ * @author Alexander Maza
+ */
+
+import ec.edu.espe.model.UsTax;
+import ec.edu.espe.view.TaxView;
+import ec.edu.espe.controller.TaxController;
+
+import java.util.Scanner;
+
+public class Singleton {
+    public static void main(String[] args) {
+        UsTax taxModel = UsTax.getInstance();
+        TaxView taxView = new TaxView();
+        TaxController controller = new TaxController(taxModel, taxView);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the amount: ");
+        double amount = scanner.nextDouble();
+
+        controller.calculateAndShowTotal(amount);
+    }
+}
