5ed35afe7722fcee143d7c9c3418b6259b62d587
unknown <mateotopon7@gmail.com>
2025-02-20 07:56:37 -0500

    Strategy Pattern

--- hw/topon/U3/Hw 37 Strategy Pattern/src/main/java/ec/edu/espe/strategypattern/model/BubbleSort.java
+++ hw/topon/U3/Hw 37 Strategy Pattern/src/main/java/ec/edu/espe/strategypattern/model/BubbleSort.java
@@ -10,7 +10,7 @@ public class BubbleSort implements SortingStrategy {
         int n = data.length;
         int temp;
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) { 
+            for (int j = 0; j < n - i - 1; j++) {
                 if (data[j] > data[j + 1]) {
                     temp = data[j];
                     data[j] = data[j + 1];
--- hw/topon/U3/Hw 37 Strategy Pattern/src/main/java/ec/edu/espe/strategypattern/view/SortApp.java
+++ hw/topon/U3/Hw 37 Strategy Pattern/src/main/java/ec/edu/espe/strategypattern/view/SortApp.java
@@ -8,7 +8,7 @@ import ec.edu.espe.strategypattern.model.SortingContext;
  */
 public class SortApp {
     public static void main(String args[]) {
-        int data[] = {3, 3, 6, 7, 8, 8, 6, 6, 6, 6, 4, 6, 4, 6, 6, 7, 7, 8, 5, 6, 7, 5, 3, 3, 3};
+        int data[] = {3, 3, 6, 7, 8, 8, 6, 6, 6, 6, 4, 6, 4, 6, 6, 7, 8, 5, 6, 7, 5, 3, 3, 3};
         SortingContext sc = new SortingContext();
         int sortedList[] = sc.sort(data);
 
