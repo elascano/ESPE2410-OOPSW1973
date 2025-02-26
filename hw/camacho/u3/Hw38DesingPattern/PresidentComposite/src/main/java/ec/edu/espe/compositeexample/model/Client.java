2d0f8e5b3ef77730ca5525d21833275dfed600ae
Mateo Ismael Camacho Ponce <mateomarucha@gmail.com>
2025-02-26 17:11:27 -0500

    hw

---
+++ hw/camacho/u3/Hw38DesingPattern/PresidentComposite/src/main/java/ec/edu/espe/compositeexample/model/Client.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.compositeexample.model;
+
+/**
+ *
+ * @author Mateo
+ */
+public class Client {
+    public static Employee employee;
+    public static void doClientTasks(){
+        employee.stateName();
+    }
+}
