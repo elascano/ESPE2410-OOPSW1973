9bb070aef9d0d82a8e892713d85ac85f67aef3fe
Gabriel <gabrielmanosalvas79@gmail.com>
2025-02-27 18:58:21 -0500

    Add hw

---
+++ hw/manosalvas/U3/HW38CompositeFactoryTemplate/CompositePattern/src/main/java/ec/edu/espe/compositepattern/view/Teller.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.compositepattern.view;
+
+/**
+ *
+ * @author Gabriel Manosalvas
+ */
+public class Teller extends Employee {
+    public Teller(String aName) {
+        this.name = aName;
+        this.title = "Teller";
+    }
+}
---
+++ hw/manosalvas/U3/HW38CompositeFactoryTemplate/TemplateExample/src/main/java/ec/edu/espe/templateexample/model/CaffeineBeverage.java
@@ -0,0 +1,23 @@
+
+package ec.edu.espe.templateexample.model;
+
+/**
+ *
+ * @author Gabriel Manosalvas
+ */
+public abstract class CaffeineBeverage {
+    public void prepareRecipe(){ //template method
+        boilWater();
+        brew();
+        pourInCup();
+        if(wantsCondiments()){
+            addCondiments();
+        }
+    }
+    
+    void boilWater(){System.out.println("Boiling water");}
+    abstract void brew();
+    void pourInCup(){System.out.println("Pouring into cup");}
+    abstract void addCondiments();
+    boolean wantsCondiments(){return true;} //a hook operation
+}
