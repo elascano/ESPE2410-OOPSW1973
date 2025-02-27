bbcb3e83e4db26c53b757cf710b0dec5cab69848
LizTipan <lisbethtipan47@gmail.com>
2025-02-27 16:32:45 -0500

    Modification2

---
+++ ws/tipan/U2/Ws14Libraries/Calculator/src/main/java/ec/espe/edu/calculator/controller/BasicOperationController.java
@@ -0,0 +1,42 @@
+package ec.espe.edu.calculator.controller;
+
+import ec.espe.edu.calculator.model.BasicOperation;
+
+/**
+ *
+ * @author Mateo Topon
+ */
+public class BasicOperationController {
+    
+    private BasicOperation basicOperation;
+    
+    public float add(float addend1,float addend2){
+        float sum;
+        sum= addend1 + addend2;
+        return sum;
+        
+    }
+    public float add(){
+        float sum;
+        sum=basicOperation.getTerm1()+ basicOperation.getTerm2();
+        return sum;
+    }
+    public BasicOperationController(BasicOperation basicOperation) {
+        this.basicOperation = basicOperation;
+    }
+
+    /**
+     * @return the basicOperation
+     */
+    public BasicOperation getBasicOperation() {
+        return basicOperation;
+    }
+
+    /**
+     * @param basicOperation the basicOperation to set
+     */
+    public void setBasicOperation(BasicOperation basicOperation) {
+        this.basicOperation = basicOperation;
+    }
+    
+}
---
+++ ws/tipan/U2/Ws14Libraries/Calculator/src/main/java/ec/espe/edu/calculator/model/BasicOperation.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.espe.edu.calculator.model;
+
+/**
+ *
+ * @author Mateo Topon
+ */
+public class BasicOperation {
+    private float term1;
+    private float term2;
+
+    public BasicOperation(float term1, float term2) {
+        this.term1 = term1;
+        this.term2 = term2;
+    }
+
+    /**
+     * @return the term1
+     */
+    public float getTerm1() {
+        return term1;
+    }
+
+    /**
+     * @param term1 the term1 to set
+     */
+    public void setTerm1(float term1) {
+        this.term1 = term1;
+    }
+
+    /**
+     * @return the term2
+     */
+    public float getTerm2() {
+        return term2;
+    }
+
+    /**
+     * @param term2 the term2 to set
+     */
+    public void setTerm2(float term2) {
+        this.term2 = term2;
+    }
+    
+}
