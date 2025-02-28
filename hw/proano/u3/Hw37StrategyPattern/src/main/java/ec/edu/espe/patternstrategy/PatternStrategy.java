68e3f00b066799bbe6d7c8a205d6f2a86690b917
ABNER FABIAN PROAÑO QUISHPE <abnerproanio@gmail.com>
2025-02-28 09:41:30 -0500

    Add files via upload

---
+++ hw/proano/u3/Hw37StrategyPattern/src/main/java/ec/edu/espe/patternstrategy/PatternStrategy.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.patternstrategy;
+
+/**
+ *
+ * @author abner
+ */
+public class PatternStrategy {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
