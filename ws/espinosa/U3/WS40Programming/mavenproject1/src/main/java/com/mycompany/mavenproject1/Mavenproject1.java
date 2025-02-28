3e9f538a16f773507689bcd7c499c9b3e16996cc
Andres-Espinosa-18 <aeespinosa6@espe.edu.ec>
2025-02-28 13:11:55 -0500

    WS

---
+++ ws/espinosa/U3/WS40Programming/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Andrés Espinosa
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
